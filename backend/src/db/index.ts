9df322c5e916987fd2c8240b123d0173b4643e44
sfgi-ap <inve2050@gmail.com>
2026-06-18 20:30:59 -0400

    cambios

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,9 +1,14 @@
 import "dotenv/config";
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from "pg";
-
 import * as schema from "./schema.js";
 
-const pool= new pg.Pool({connectionString: process.env.DATABASE_URL});
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
 
-export const db = drizzle(pool,{schema})
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+export const db = drizzle(pool, { schema });
